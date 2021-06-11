61d3e68cfae243174135d9633748ac99ea52bac5
Sasha Skryd <sashaskryd@gmail.com>
2021-06-11 19:05:36 +0300

    ruler complete

--- src/components/Ruler/Ruler.jsx
+++ src/components/Ruler/Ruler.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import './Ruler.scss'
 
+const time = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
-function rulerPart(time){
+function rulerPart(el){
     return <li className="first-point">
-
-        <span className='time-indicator'>{time.length <= 1 ? `0${time}:00` : `${time}:00`}</span>
-
-        <ul>
-            <li className="small-point"></li>
-            <li className="small-point"></li>
-            <li className="medium-point"></li>
-            <li className="small-point"></li>
-            <li className="small-point"></li>
-            <li className="high-point"></li>
-            <li className="small-point"></li>
-            <li className="small-point"></li>
-            <li className="medium-point"></li>
-            <li className="small-point"></li>
-            <li className="small-point"></li>
-        </ul>
-    </li>
+    <span className='time-indicator'>{el.toString().length === 1 ? `0${el}:00` : `${el}:00`}</span>
+    <ul>
+        <li className="small-point"></li>
+        <li className="small-point"></li>
+        <li className="medium-point"></li>
+        <li className="small-point"></li>
+        <li className="small-point"></li>
+        <li className="high-point"></li>
+        <li className="small-point"></li>
+        <li className="small-point"></li>
+        <li className="medium-point"></li>
+        <li className="small-point"></li>
+        <li className="small-point"></li>
+        <li className="main-point"></li>
+    </ul>
+</li>
 };
 
-function rulerRender (){
-    
-let ruler  = ''
-
-    for(let i = 0;i<24;i++){
-        
-       ruler += rulerPart(i);
-    }
-    console.dir(ruler,'Ruler');
-
-    return ruler
-}
-
 export default function Ruler (){
-
     return(
         <div className='ruler-wraper'>
             <ul className='ruler'>
-                {rulerRender ()}
-            </ul>
+                {time.map(el=>
+                   rulerPart(el)
+                )}
+            </ul>      
         </div>
     );
 };
+
